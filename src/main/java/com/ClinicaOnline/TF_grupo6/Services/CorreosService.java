b3c8f03d5a5d7f2be75eccd8046901eac264ae05
David Chávez <davidechg2965@gmail.com>
2025-11-22 19:29:02 -0500

    Update CorreosService.java

--- src/main/java/com/ClinicaOnline/TF_grupo6/Services/CorreosService.java
+++ src/main/java/com/ClinicaOnline/TF_grupo6/Services/CorreosService.java
@@ -1,55 +1,71 @@
 package com.ClinicaOnline.TF_grupo6.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import javax.mail.*;
+import javax.mail.internet.*;
 import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 @Service
 public class CorreosService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${SPRING_MAIL_USERNAME}")
+    private String remitente;
+
+    @Value("${SPRING_MAIL_PASSWORD}")
+    private String contrasena;
 
     public void enviarCorreo(String destinatario, String asunto, String cuerpo, String imagenRuta) {
 
-        try {
-            MimeMessage mensaje = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mensaje, true);
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-            helper.setTo(destinatario);
-            helper.setSubject(asunto);
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(remitente, contrasena);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(remitente));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            message.setSubject(asunto);
 
             String html = "<html><body>"
-                    + "<h3>" + cuerpo + "</h3>";
+                    + "<h3>" + cuerpo + "</h3>"
+                    + "<img src='cid:imagen_embebida' />"
+                    + "</body></html>";
+
+            MimeBodyPart htmlParte = new MimeBodyPart();
+            htmlParte.setContent(html, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(htmlParte);
 
             if (imagenRuta != null) {
-                html += "<img src='cid:imagen_embebida'>";
-            }
-
-            html += "</body></html>";
-
-            helper.setText(html, true);
-
-            if (imagenRuta != null) {
-                FileSystemResource img = new FileSystemResource(new File(imagenRuta));
-                if (img.exists()) {
-                    helper.addInline("imagen_embebida", img);
+                File imagen = new File(imagenRuta);
+                if (imagen.exists()) {
+                    MimeBodyPart imagenParte = new MimeBodyPart();
+                    imagenParte.attachFile(imagen);
+                    imagenParte.setContentID("<imagen_embebida>");
+                    imagenParte.setDisposition(MimeBodyPart.INLINE);
+                    multipart.addBodyPart(imagenParte);
                 }
             }
 
-            helper.setFrom(System.getenv("SPRING_MAIL_USERNAME"));
+            message.setContent(multipart);
+            Transport.send(message);
 
-            mailSender.send(mensaje);
+            System.out.println("Correo enviado a " + destinatario);
 
-            System.out.println("Correo enviado correctamente a " + destinatario);
-
-        } catch (MessagingException e) {
+        } catch (MessagingException | IOException e) {
             e.printStackTrace();
             System.out.println("Error enviando correo: " + e.getMessage());
         }
