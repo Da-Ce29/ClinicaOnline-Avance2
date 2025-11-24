a40dba14339616d1ccecce35fc1edeb83a5e4260
David Chávez <davidechg2965@gmail.com>
2025-11-24 10:32:25 -0500

    Update PacientesRepository.java

--- src/main/java/com/ClinicaOnline/TF_grupo6/Repositorys/PacientesRepository.java
+++ src/main/java/com/ClinicaOnline/TF_grupo6/Repositorys/PacientesRepository.java
@@ -4,4 +4,7 @@ import com.ClinicaOnline.TF_grupo6.Entitys.Pacientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacientesRepository extends JpaRepository<Pacientes, Long> {
+  boolean existsByCorreo(String correo);        // Para validar existencia
+  Pacientes findByCorreo(String correo);       // Para recuperar paciente existente
+}
 }
