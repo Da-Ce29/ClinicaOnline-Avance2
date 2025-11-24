119a66577bd78fdf14fa53384bc8f5121390cf04
David Chávez <davidechg2965@gmail.com>
2025-11-24 11:22:34 -0500

    Update TfGrupo6Application.java

--- src/main/java/com/ClinicaOnline/TF_grupo6/TfGrupo6Application.java
+++ src/main/java/com/ClinicaOnline/TF_grupo6/TfGrupo6Application.java
@@ -2,8 +2,10 @@ package com.ClinicaOnline.TF_grupo6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TfGrupo6Application {
     public static void main(String[] args) {
         SpringApplication.run(TfGrupo6Application.class, args);
